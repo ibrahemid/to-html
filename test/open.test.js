7a5973d9f8596d28b05e7aedcfb0796c9afffede
ibrahemid <ibrahemid@gmail.com>
2026-05-26 17:03:03 +0800

    fix(to-html): Windows file:// URL (F6); gitignore coverage; sync marketplace version; dedup writeFileAtomic; drop dead exports

---
+++ test/open.test.js
@@ -0,0 +1,17 @@
+'use strict';
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const { pathToFileUrl } = require('../lib/open');
+
+test('pathToFileUrl: posix path encodes spaces but keeps slashes', () => {
+  assert.equal(pathToFileUrl('/Users/x/my file.html', 'darwin'), 'file:///Users/x/my%20file.html');
+});
+test('pathToFileUrl: windows drive path keeps the drive colon, uses forward slashes', () => {
+  assert.equal(pathToFileUrl('C:\\Users\\foo\\file.html', 'win32'), 'file:///C:/Users/foo/file.html');
+});
+test('pathToFileUrl: windows path with spaces encodes them, colon preserved', () => {
+  assert.equal(pathToFileUrl('C:\\My Files\\a.html', 'win32'), 'file:///C:/My%20Files/a.html');
+});
+test('pathToFileUrl: windows UNC path uses host form', () => {
+  assert.equal(pathToFileUrl('\\\\server\\share\\a.html', 'win32'), 'file://server/share/a.html');
+});
